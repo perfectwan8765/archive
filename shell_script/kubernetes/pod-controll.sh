1ccd93a1e031438cfdff57425e7c8e55a7c7a00c
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2023-07-24 20:51:46 +0900

    ADD pod list using pvc

--- shell_script/kubernetes/pod-controll.sh
+++ shell_script/kubernetes/pod-controll.sh
@@ -1,2 +1,5 @@
 # delete pod status Error (ex. namespace airflow)
 kubectl get pod -o wide -n airflow | awk '$3 == "Error" {print $1 " -n airflow"}' | xargs kubectl delete pod
+
+# get pod list using pvc
+kubectl get pods --all-namespaces -o=json | jq -c '.items[] | {name: .metadata.name, namespace: .metadata.namespace, claimName: .spec |  select( has ("volumes") ).volumes[] | select( has ("persistentVolumeClaim") ).persistentVolumeClaim.claimName }'
