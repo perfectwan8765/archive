21a54050129c54bab4cafcb6a18f22838b0daed6
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2023-08-01 21:34:47 +0900

    Update add field-selector

--- shell_script/kubernetes/pod-controll.sh
+++ shell_script/kubernetes/pod-controll.sh
@@ -3,3 +3,9 @@ kubectl get pod -o wide -n airflow | awk '$3 == "Error" {print $1 " -n airflow"}
 
 # get pod list using pvc
 kubectl get pods --all-namespaces -o=json | jq -c '.items[] | {name: .metadata.name, namespace: .metadata.namespace, claimName: .spec |  select( has ("volumes") ).volumes[] | select( has ("persistentVolumeClaim") ).persistentVolumeClaim.claimName }'
+
+# pod status complete
+kubectl get pods --field-selector=status.phase=Succeeded -n cvat
+
+# pod status containerunknown, error
+kubectl get pods --field-selector=status.phase=Failed -n cvat
