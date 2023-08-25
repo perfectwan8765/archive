019dc8cdbc9265014334205b408f59a4d4def58e
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2023-08-25 15:54:30 +0900

    UPDATE add health, pg scrub check

--- shell_script/ceph/admin-cli.sh
+++ shell_script/ceph/admin-cli.sh
@@ -1,2 +1,8 @@
+# ceph health check
+ceph health detail
+
 # ceph osd config update
-cepth tell 'osd.*' injectargs --osd-max-backfills=2 --osd-recovery-max-active=6
+ceph tell 'osd.*' injectargs --osd-max-backfills=2 --osd-recovery-max-active=6
+
+# pg scrub check
+ceph pg ${pg} query | jq .scrubber 
