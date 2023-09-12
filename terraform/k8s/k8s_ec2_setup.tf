03f0b3bdff304b0c021ceb1dd217099f5ca6c7a7
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2023-09-12 17:09:17 +0900

    UPDATE AWS k8s terraform

--- terraform/k8s/k8s_ec2_setup.tf
+++ terraform/k8s/k8s_ec2_setup.tf
@@ -17,28 +17,38 @@ variable "nodes" {
   type   = map(object({
     name = string
     type = string
-    size = number
+    docker_size = number
+    volume_size = number
+    public_ip = bool
   }))
   default = {
     "k8smaster" = {
       name = "k8smaster" 
       type = "t3.large" 
-      size = "20" 
+      size = 40
+      volume_size = 0
+      public_ip = true
     },
     "k8sworker1" = {
       name = "k8sworker1" 
       type = "t3.xlarge" 
-      size = "40" 
+      docker_size = 40
+      volume_size = 100
+      public_ip = false 
     },
     "k8sworker2" = {
       name = "k8sworker2" 
       type = "t3.xlarge" 
-      size = "40" 
+      docker_size = 40
+      volume_size = 100
+      public_ip = false 
     },
     "k8sworker3" = {
       name = "k8sworker3" 
       type = "t3.xlarge" 
-      size = "40" 
+      docker_size = 40
+      volume_size = 100
+      public_ip = false 
     }
   }
 }
@@ -58,7 +68,7 @@ resource "aws_instance" "instances" {
 
   root_block_device {
     volume_type = "gp3" 
-    volume_size = 10 # GiB
+    volume_size = 15 # GiB
   }
 
   tags = {
@@ -67,11 +77,11 @@ resource "aws_instance" "instances" {
   }
 }
 
-resource "aws_ebs_volume" "volumes" {
+resource "aws_ebs_volume" "docker_volumes" {
   for_each = var.nodes
 
   availability_zone = "ap-northeast-2c" 
-  size = each.value.size
+  size = each.value.docker_size
   type = "gp3" 
 
   tags = {
@@ -80,7 +90,7 @@ resource "aws_ebs_volume" "volumes" {
   }
 }
 
-resource "aws_volume_attachment" "attachements" {
+resource "aws_volume_attachment" "docker_attachements" {
   for_each = var.nodes
 
   device_name = "/dev/sdh" 
@@ -88,7 +98,39 @@ resource "aws_volume_attachment" "attachements" {
   instance_id = aws_instance.instances[each.value.name].id
 }
 
+resource "aws_ebs_volume" "data_volumes" {
+  for_each = {
+    for name, node in var.nodes : name => node
+    if node.volume_size != 0
+  }
+
+  availability_zone = "ap-northeast-2c"
+  size = each.value.volume_size
+  type = "gp3"
+
+  tags = {
+    Name = "${each.value.name}-data-vol"
+    Provider = "terraform"
+    Group = "k8scluster"
+  }
+}
+
+resource "aws_volume_attachment" "data_attachements" {
+  for_each = {
+    for name, node in var.nodes : name => node
+    if node.volume_size != 0
+  }
+
+  device_name = "/dev/sdj"
+  volume_id   = aws_ebs_volume.data_volumes[each.value.name].id
+  instance_id = aws_instance.instances[each.value.name].id
+}
+
 resource "aws_eip" "master_ip" {
+  for_each = {
+    for name, node in var.nodes : name => node
+    if node.public_ip == true
+  }
   instance = aws_instance.instances["k8smaster"].id
   vpc      = true
 
