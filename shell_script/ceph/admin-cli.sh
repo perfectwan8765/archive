940f9eb35744ce22f31527259eb894c4e6b3889d
SANGWAN <38679417+perfectwan8765@users.noreply.github.com>
2023-12-28 15:51:30 +0900

    UPDATE ceph-admin-cli.sh
    1) ADD ceph crash situation

--- shell_script/ceph/admin-cli.sh
+++ shell_script/ceph/admin-cli.sh
@@ -6,3 +6,11 @@ ceph tell 'osd.*' injectargs --osd-max-backfills=2 --osd-recovery-max-active=6
 
 # pg scrub check
 ceph pg ${pg} query | jq .scrubber 
+
+# crash situation
+ceph crash ls
+
+ceph crash info ${crash_id}
+
+ceph crash archive ${crash_id} 
+ceph crash archive-all
